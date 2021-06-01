b71ea4131213eac25052f4b19bc3c4f911eeded3
Harrison <32177072+harrisonchiu@users.noreply.github.com>
2021-06-01 14:40:16 -0400

    Renamed dog breed folders so breed names can be searched easily
    Properly renamed training_set folders

---
+++ images/README.md
@@ -0,0 +1,5 @@
+images and files (not folders) that have a `n` as a prefix in the file name `n*_*` is from the stanford dog dataset.
+
+`u` prefix is from the udacity dog dataset `https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip`
+
+note that some images may be repeated
---
+++ images/rename_image_set_folders.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Renames the folders in training_set/ and validation_set/ directories
+so that they can be sorted alphabetically instead of by breed id
+
+Example: n*-dog_breed_name/ -> dog_breed_name-n*/
+"""
+
+import os
+import sys
+
+CURRENT_DIR = os.getcwd()
+TRAINING_SET_DIR = os.path.join(CURRENT_DIR, "training_set")
+VALIDATION_SET_DIR = os.path.join(CURRENT_DIR, "validation_set")
+
+def rename_folders(directory):
+    for breed_folder in os.listdir(directory):
+        breed_id = breed_folder[:breed_folder.find("-")]
+        breed_name = breed_folder[breed_folder.find("-") + 1:]
+        breed_folder = os.path.join(directory, breed_folder)
+
+        if os.path.exists(breed_folder) and breed_id[0] == "n" and breed_id[1].isdigit():
+            os.rename(breed_folder, os.path.join(directory, breed_name + "-" + breed_id))
+
+# rename_folders(TRAINING_SET_DIR)
+# rename_folders(VALIDATION_SET_DIR)
+
+def output_stanford_breed_names():
+    f = open("stanford_breeds", "w")
+    for breed_folder in os.listdir(TRAINING_SET_DIR):
+        f.write(breed_folder[breed_folder.find("-") + 1:] + "\n")
+
+    f.close()
+
+# output_stanford_breed_names()
